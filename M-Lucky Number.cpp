d544150336da76a86a42d4909fc39d20334e78f2
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2020-07-12 00:18:38 +0600

    STL set
    https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/M

---
+++ M-Lucky Number.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    while(cin>>a>>b)
+    {
+        int count=0;
+        bool flagCount = false;
+
+        for(int i=a; i<=b; i++)
+        {
+            set<char> st;
+            string s = to_string(i);
+            bool flag = true;
+            count=0;
+
+            if(s.size()==1)
+            {
+                if(s[0]=='4') {cout<<"4 "; count++; flagCount=true;}
+                else if(s[0]=='7') {cout<<"7 "; count++; flagCount=true;}
+            }
+            else
+            {
+                for(int j = 0; j<s.size(); j++)
+                {
+                    if(s[j]=='4' || s[j]=='7') st.insert(s[j]);
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if((st.size()==1 || st.size()==2) && flag==true){cout<<s<<" "; count++; flagCount=true;}
+            }
+        }
+        if(count==0 && flagCount==false) cout<<"-1";
+        cout<<endl;
+    }
+}
